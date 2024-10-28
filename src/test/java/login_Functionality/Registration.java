e0ec4f3a86f2de9bb12095367dd71cd684f50cda
NagarajuThulluru <thullurunagaraju9@gmail.com>
2024-10-28 16:08:36 +0530

    i added one registration class

---
+++ src/test/java/login_Functionality/Registration.java
@@ -0,0 +1,14 @@
+package login_Functionality;
+
+public class Registration {
+
+	public static void main(String[] args) {
+		int a=10;
+		int b=20;
+		int c=a-b;
+		System.out.println(c);
+		System.out.println("registration completed");
+
+	}
+
+}
