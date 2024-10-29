af5c9039c5db4cd5fef31030a549febd04ee8b5d
NagarajuThulluru <thullurunagaraju9@gmail.com>
2024-10-29 15:35:52 +0530

    i added 2 classes

---
+++ src/test/java/login_Functionality/Fb_login.java
@@ -0,0 +1,9 @@
+package login_Functionality;
+
+public class Fb_login {
+
+	public static void main(String[] args) {
+		System.out.println("login done we navigate the logout page");
+	}
+
+}
---
+++ src/test/java/login_Functionality/fb_registaration.java
@@ -0,0 +1,9 @@
+package login_Functionality;
+
+public class fb_registaration {
+
+	public static void main(String[] args) {
+		System.out.println("registration done  then we go for login page");
+	}
+
+}
